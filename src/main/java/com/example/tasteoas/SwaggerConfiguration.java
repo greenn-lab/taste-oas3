b46b1a3028b38ced3d9314cbffeea55cb275b5c8
green <greenn.lab@gmail.com>
2022-06-07 09:24:02 +0900

    add swagger

--- src/main/java/com/example/tasteoas/SwaggerConfiguration.java
+++ src/main/java/com/example/tasteoas/SwaggerConfiguration.java
@@ -1,2 +1,68 @@
-package com.example.tasteoas;public class SwaggerConfiguration {
+package com.example.tasteoas;
+
+import java.util.Collections;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+
+  public static final String BEARER = "Bearer";
+
+  @Bean
+  public Docket api() {
+    return new Docket(
+        DocumentationType.OAS_30)
+        .apiInfo(apiInfo())
+        .securityContexts(List.of(this.securityContext()))
+        .securitySchemes(List.of(apiKey()))
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
+
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfo(
+        "SORIZAVA REST API",
+        "Some custom description of API.",
+        "1.0",
+        "Terms of service",
+        new Contact("sorizava", "www.sorizava.com", "sorizava@sorizava.co.kr"),
+        "License of API",
+        "API license URL",
+        Collections.emptyList());
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+        .securityReferences(defaultAuth())
+        .build();
+  }
+
+
+  private List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+
+    return List.of(new SecurityReference(BEARER, authorizationScopes));
+  }
+
+  private ApiKey apiKey() {
+    return new ApiKey(BEARER, BEARER, "header");
+  }
+
 }
--- src/main/java/com/example/tasteoas/books/BookController.java
+++ src/main/java/com/example/tasteoas/books/BookController.java
@@ -1,2 +1,41 @@
-package com.example.tasteoas.books;public class BookController {
+package com.example.tasteoas.books;
+
+import java.util.Collection;
+import java.util.Collections;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/book")
+public class BookController {
+
+  @GetMapping("/{id}")
+  public Book findById(@PathVariable long id) {
+    return Book.builder()
+        .id(id)
+        .isbn("1234-5678-0912-345")
+        .title("Anna calelina")
+        .build();
+  }
+
+  @GetMapping("/")
+  public Collection<Book> findBooks() {
+    return Collections.emptyList();
+  }
+
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Book updateBook(
+      @PathVariable("id") final String id,
+      @RequestBody final Book book
+  ) {
+    return book;
+  }
+
 }
